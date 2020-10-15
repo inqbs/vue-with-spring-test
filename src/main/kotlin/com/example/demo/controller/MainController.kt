38c2f7ae01daeb544b95c05cf98c674549e0f60c
inqbs <incubass@outlook.com>
2020-10-15 17:27:29 +0900

    Test for 'vue on spring boot server'

---
+++ src/main/kotlin/com/example/demo/controller/MainController.kt
@@ -0,0 +1,28 @@
+package com.example.demo.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+import java.time.LocalDateTime
+
+import kotlin.random.Random
+
+@Controller
+class MainController {
+
+    @GetMapping("/")
+    fun main(): String {
+        return "index"
+    }
+
+    @GetMapping("/data")
+    @ResponseBody
+    fun data(): String{
+        return """{
+            'value' : ${Random.nextInt(10)},
+            'date' : '${LocalDateTime.now()}'
+        }"""
+    }
+
+}
